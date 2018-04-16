d2ae256099871bca0a5c48e36f6b8c786d3586ec
Erik Edvardsen <edvardsen79@gmail.com>
2018-04-16 20:41:12 +0200

    IUnitOfWork, UnitOfWork

---
+++ Persistence/Abstract/IStratosphereUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Persistence.Entities;
+
+namespace Persistence.Abstract
+{
+    public interface IStratosphereUnitOfWork : IDisposable
+    {
+        IRepository<Competition> Competitions { get; }
+
+        IRepository<CompetitionSetup> CompetitionSetups { get; }
+
+        IRepository<Group> Groups { get; }
+
+        IRepository<Match> Matches { get; }
+
+        IRepository<MatchDay> MatchDays { get; }
+
+        IRepository<MatchRound> MatchRounds { get; }
+
+        IRepository<MatchStatistics> MatchStatistics { get; }
+
+        IRepository<Team> Teams { get; }
+
+        IRepository<TeamOnCompetition> TeamOnCompetitions { get; }
+
+        IRepository<User> Users { get; }
+
+        IRepository<UserCompetitionScore> UserCompetitionScores { get; }
+
+        IRepository<UserLeague> UserLeagues { get; }
+
+        IRepository<UserMatchPrediction> UserMatchPredictions { get;  }
+
+        IRepository<UserOnLeague> UserOnLeagues { get; }
+
+        IRepository<UserTieBreak> UserTieBreaks { get;  }
+
+        int Complete();
+    }
+}
--- Persistence/StratosphereUnitOfWork.cs
+++ Persistence/StratosphereUnitOfWork.cs
@@ -1,10 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Persistence.Abstract;
+using Persistence.Entities;
 
 namespace Persistence
 {
-    public class StratosphereUnitOfWork
+    public class StratosphereUnitOfWork : IStratosphereUnitOfWork
     {
+        private StratosphereContext _context;
+
+        public StratosphereUnitOfWork(StratosphereContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Competitions = new Repository<Competition>(_context);
+            CompetitionSetups = new Repository<CompetitionSetup>(_context);
+            Groups = new Repository<Group>(_context);
+            Matches = new Repository<Match>(_context);
+            MatchDays = new Repository<MatchDay>(_context);
+            MatchRounds = new Repository<MatchRound>(_context);
+            MatchStatistics = new Repository<MatchStatistics>(_context);
+            Teams = new Repository<Team>(_context);
+            TeamOnCompetitions = new Repository<TeamOnCompetition>(_context);
+            Users = new Repository<User>(_context);
+            UserCompetitionScores = new Repository<UserCompetitionScore>(_context);
+            UserLeagues = new Repository<UserLeague>(_context);
+            UserMatchPredictions = new Repository<UserMatchPrediction>(_context);
+            UserOnLeagues = new Repository<UserOnLeague>(_context);
+            UserTieBreaks = new Repository<UserTieBreak>(_context);
+        }
+
+        public IRepository<Competition> Competitions { get; private set; }
+        public IRepository<CompetitionSetup> CompetitionSetups { get; private set; }
+        public IRepository<Group> Groups { get; private set; }
+        public IRepository<Match> Matches { get; private set; }
+        public IRepository<MatchDay> MatchDays { get; private set; }
+        public IRepository<MatchRound> MatchRounds { get; private set; }
+        public IRepository<MatchStatistics> MatchStatistics { get; private set; }
+        public IRepository<Team> Teams { get; private set; }
+        public IRepository<TeamOnCompetition> TeamOnCompetitions { get; private set; }
+        public IRepository<User> Users { get; private set; }
+        public IRepository<UserCompetitionScore> UserCompetitionScores { get; private set; }
+        public IRepository<UserLeague> UserLeagues { get; private set; }
+        public IRepository<UserMatchPrediction> UserMatchPredictions { get; private set; }
+        public IRepository<UserOnLeague> UserOnLeagues { get; private set; }
+        public IRepository<UserTieBreak> UserTieBreaks { get; private set; }
+
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
